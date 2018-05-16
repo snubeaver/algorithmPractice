f8161f856893b1fbcba5721bc384acad8acc58ce
Yesjin <yesjin_nav@naver.com>
2018-05-16 22:46:19 +0900

    5.16

---
+++ new/B1152.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class B1152 {
+	public static void main (String[] args){
+		Scanner scan = new Scanner(System.in);
+		String line = scan.nextLine();
+
+		String[] b = line.split(" ");
+		System.out.println(b.length);
+	}
+}
